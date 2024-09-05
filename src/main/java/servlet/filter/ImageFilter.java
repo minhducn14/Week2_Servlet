afe490486351352d1176cbf0fc32a7d56222886e
unknown <vunguyenminhduc1410@gmail.com>
2024-09-05 12:14:50 +0700

    Sucess authentication filter and image filter

---
+++ src/main/java/servlet/filter/ImageFilter.java
@@ -0,0 +1,78 @@
+package servlet.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.annotation.WebInitParam;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.http.HttpRequest;
+
+/**
+ * Servlet Filter implementation class ImageFilter
+ */
+@WebFilter(urlPatterns = { "*.png", "*.jpg", "*.gif" }, initParams = {
+		@WebInitParam(name = "notFoundImage", value = "/images/image-not-found.png") })
+public class ImageFilter extends HttpFilter implements Filter {
+	private String notFoundImage;
+
+	/**
+	 * @see HttpFilter#HttpFilter()
+	 */
+	public ImageFilter() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletRequest req = (HttpServletRequest) request;
+
+		String servletPath = req.getServletPath();
+		System.out.println(servletPath);
+		String realRootPath = request.getServletContext().getRealPath("");
+		System.out.println(realRootPath);
+		String imageRealPath = realRootPath + servletPath;
+
+		System.out.println("imageRealPath = " + imageRealPath);
+
+		File file = new File(imageRealPath);
+		if (file.exists()) {
+			chain.doFilter(request, response);
+
+		} else if (!servletPath.equals(this.notFoundImage)) {
+			HttpServletResponse resp = (HttpServletResponse) response;
+			resp.sendRedirect(req.getContextPath() + this.notFoundImage);
+
+		}
+
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		notFoundImage = fConfig.getInitParameter("notFoundImage");
+	}
+
+}
